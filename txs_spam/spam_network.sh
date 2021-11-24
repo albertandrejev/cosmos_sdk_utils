a257cf2acda33cfd40d64830c6d07635085b55d0
Albert Andrejev <albert.andrejev@gmail.com>
2021-11-24 18:53:08 +0200

    Script update

--- txs_spam/spam_network.sh
+++ txs_spam/spam_network.sh
@@ -10,13 +10,29 @@ DENOM=${7}
 SEND_AMOUNT=${8:-200}
 FEE_AMOUNT=${9:-200}
 NODE=${10:-"http://localhost:26657"}
+PERIOD_VALUE=${11:-100}
+
+ROUND=0
+BROADCAST_MODE="async" 
 
 SEQ=$(${PATH_TO_SERVICE} q account ${ACCOUNT} -o json | jq '.sequence | tonumber')
 
 while :
 do
+    PERIOD=`expr ${ROUND} % ${PERIOD_VALUE}`
+
     echo "Running sequence: ${SEQ}"
     CURRENT_BLOCK=$(curl -s ${NODE}/abci_info | jq -r .result.response.last_block_height)
+
+    if (( $PERIOD == 1 )); then
+        BROADCAST_MODE="sync"
+        echo "Sync broadcast mode"
+    fi
+
+    if (( $PERIOD == 4 )); then
+        BROADCAST_MODE="async"
+        echo "Async broadcast mode"
+    fi
 
     TX_RESULT_RAW_LOG=$(echo $KEY_PASSWORD | $PATH_TO_SERVICE tx bank send $ACCOUNT $TO_ADDRESS \
         ${FEE_AMOUNT}${DENOM} \
@@ -24,8 +40,8 @@ do
         --chain-id $CHAIN_ID \
         --output json \
         --note $MEMO \
-        --broadcast-mode async \
-        -s $SEQ \
+        --broadcast-mode $BROADCAST_MODE \
+        --sequence $SEQ \
         --timeout-height $(($CURRENT_BLOCK + 5)) -y | \
         jq '.raw_log')
 
@@ -33,8 +49,10 @@ do
 
     if [[ "$TX_RESULT_RAW_LOG" == *"incorrect account sequence"* ]]; then
         echo $TX_RESULT_RAW_LOG
-        sleep 7
+        sleep 28
         SEQ=$(echo $TX_RESULT_RAW_LOG | sed 's/.* expected \([0-9]*\).*/\1/')
         echo $SEQ
     fi
+
+    ROUND=`expr ${ROUND} + 1`
 done
