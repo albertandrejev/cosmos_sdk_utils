d976522551e1347e0a09351b1c0701a4f242e937
Albert Andrejev <albert.andrejev@gmail.com>
2021-12-30 22:22:22 +0200

    Validator collected commission

---
+++ validator_total_commission.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PATH_TO_SERVICE=$1
+NETWORK=$2
+VALIDATOR_ADDR=$3
+DENOM=$4
+NODE=${5:-"http://localhost:26657"}
+
+VALIDATOR_COMMISSION=$(${PATH_TO_SERVICE} q distribution commission $VALIDATOR_ADDR --node ${NODE} -o json | \
+    /usr/bin/jq ".commission[] | select(.denom | contains(\"${DENOM}\")).amount | tonumber")
+
+
+#echo $VALIDATOR_COMMISSION
+echo "opentech_cosmos_total_commission{network=\"${NETWORK}\"} $VALIDATOR_COMMISSION" > /var/lib/node_exporter/opentech_cosmos_total_commission.prom
+
