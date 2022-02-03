ea0a1bb99db0956159eb73f357948cd373829941
Albert Andrejev <albert.andrejev@gmail.com>
2022-02-03 13:43:31 +0200

    check node status

--- redelegate/redelegate.sh
+++ redelegate/redelegate.sh
@@ -12,6 +12,35 @@ NODE=${9:-"http://localhost:26657"}
 
 cd $(dirname "$0")
 
+network_up_and_synced () {
+    local NODE=$1
+
+    local NODE_STATUS_CODE=$(curl -m 5 -o /dev/null -s -w "%{http_code}\n" $NODE/status)
+
+    if (( $NODE_STATUS_CODE != 200 )); then
+        exit 1 # Node is not reacahble
+    fi
+
+    local CHAIN_STATUS=$(curl -s ${NODE}/status)
+    local CHAIN_ID=$(echo $CHAIN_STATUS | jq -r '.result.node_info.network')
+
+    local CHAIN_SYNC_STATE=$(echo $CHAIN_STATUS | jq '.result.sync_info.catching_up')
+    if [[ "$CHAIN_SYNC_STATE" == "true" ]]
+    then
+        exit 2 # Node is catching up
+    fi
+
+    local LATEST_BLOCK_TIME=$(echo $CHAIN_STATUS | jq -r '.result.sync_info.latest_block_time')
+
+    local CONTROL_TIME=$(date -d "-180 seconds")
+    local BLOCK_TIME=$(date -d "${LATEST_BLOCK_TIME}")
+
+    if [[ "$BLOCK_TIME" < "$CONTROL_TIME" ]];
+    then
+        exit 3 # Block is in the past
+    fi 
+}
+
 get_chain_id () {
     local NODE=$1
 
@@ -27,6 +56,7 @@ get_key_address () {
 
 echo "Starting new redelegation for ${DELEGATOR_ADDRESS}: "`date`" ========================="
 
+network_up_and_synced $NODE
 get_chain_id $NODE
 get_key_address $PATH_TO_SERVICE $KEY
 
--- validator_total_commission.sh
+++ validator_total_commission.sh
@@ -6,6 +6,37 @@ NETWORK=$3
 DENOM=$4
 NODE=${5:-"http://localhost:26657"}
 
+network_up_and_synced () {
+    local NODE=$1
+
+    local NODE_STATUS_CODE=$(curl -m 5 -o /dev/null -s -w "%{http_code}\n" $NODE/status)
+
+    if (( $NODE_STATUS_CODE != 200 )); then
+        exit 1 # Node is not reacahble
+    fi
+
+    local CHAIN_STATUS=$(curl -s ${NODE}/status)
+    local CHAIN_ID=$(echo $CHAIN_STATUS | jq -r '.result.node_info.network')
+
+    local CHAIN_SYNC_STATE=$(echo $CHAIN_STATUS | jq '.result.sync_info.catching_up')
+    if [[ "$CHAIN_SYNC_STATE" == "true" ]]
+    then
+        exit 2 # Node is catching up
+    fi
+
+    local LATEST_BLOCK_TIME=$(echo $CHAIN_STATUS | jq -r '.result.sync_info.latest_block_time')
+
+    local CONTROL_TIME=$(date -d "-180 seconds")
+    local BLOCK_TIME=$(date -d "${LATEST_BLOCK_TIME}")
+
+    if [[ "$BLOCK_TIME" < "$CONTROL_TIME" ]];
+    then
+        exit 3 # Block is in the past
+    fi 
+}
+
+network_up_and_synced $NODE
+
 VALIDATOR_COMMISSION=$(${PATH_TO_SERVICE} q distribution commission $VALIDATOR_ADDR --node ${NODE} -o json | \
     /usr/bin/jq ".commission[] | select(.denom | contains(\"${DENOM}\")).amount | tonumber")
 
