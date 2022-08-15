6b7e464442252430a028eaae4824cbcb76f41972
Albert Andrejev <albert.andrejev@gmail.com>
2022-08-15 23:27:02 +0300

    Add support of delegations and rewards

--- address_state/address_state.sh
+++ address_state/address_state.sh
@@ -4,8 +4,10 @@ NAME=$1
 ADDRESS=$2
 DENOM=$3
 DIVIDER=$4
-METRIC_FILE=$5
-NODE_API_URL=${6:-"http://localhost:1317"}
+WITH_REWARDS=$5
+WITH_DELEGATIONS=$6
+METRIC_FILE=$7
+NODE_API_URL=${8:-"http://localhost:1317"}
 
 cd $(dirname "$0")
 
@@ -30,6 +32,36 @@ network_up_and_synced $NODE_API_URL
 
 ADDRESS_STATE=$(curl -m 30 -s ${NODE_API_URL}/bank/balances/${ADDRESS} | \
     /usr/bin/jq -r ".result[] | select(.denom | contains(\"${DENOM}\")).amount" | xargs)
+
+if [ $WITH_REWARDS == "true" ]; then
+    sleep 5
+    REWARDS=$(curl -m 30 -s ${NODE_API_URL}/cosmos/distribution/v1beta1/delegators/${ADDRESS}/rewards | \
+        /usr/bin/jq -r ".total[] | select(.denom | contains(\"${DENOM}\")).amount" | xargs)
+
+    if [ ! -z "$REWARDS" ]
+    then
+        ADDRESS_STATE=$(echo "${ADDRESS_STATE} + ${REWARDS}" | bc)
+    fi
+fi
+
+if [ $WITH_DELEGATIONS == "true" ]; then
+    sleep 5
+    DELEGATIONS=$(curl -m 30 -s ${NODE_API_URL}/cosmos/staking/v1beta1/delegations/${ADDRESS} | \
+        /usr/bin/jq -r ".delegation_responses")
+
+    TOTAL_DELEGATIONS=$(echo "${DELEGATIONS}" | jq 'length - 1')
+
+    for DELEGATION_IDX in $( eval echo {0..$TOTAL_DELEGATIONS} )
+    do
+        DELEGATION_DATA=$(echo "${DELEGATIONS}" | jq ".[$DELEGATOR_IDX]")
+        AMOUNT=$(echo "${DELEGATION_DATA}" | jq -r ".balance.amount" | xargs)
+
+        if [ ! -z "$AMOUNT" ]
+        then
+            ADDRESS_STATE=$(echo "${ADDRESS_STATE} + ${AMOUNT}" | bc)
+        fi
+    done
+fi
 
 if [ ! -z "$ADDRESS_STATE" ]
 then
